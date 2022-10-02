86d90c9387a9ba94ff65e3206fab9a849f147834
smnet97 <smnet97@gmail.com>
2022-10-02 20:52:14 +0500

    done

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class LogoModel(models.Model):
+    logo = models.ImageField(upload_to='logo/')
+
+
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 from django.views import View
+from .models import LogoModel
 
 
 def render_to_pdf(template_src, context_data={}):
@@ -29,8 +30,33 @@ data = {
 
 class ViewPDF(View):
     def get(self, request, *args, **kwargs):
-        pdf = render_to_pdf('pdf_template.html', data)
-        return HttpResponse(pdf, 'application/pdf')
+        # pdf = render_to_pdf('pdf_template.html', data)
+        # pk = kwargs.get('pk')
+        # user = get_object_or_404(User, pk=pk)
+
+        template_path = 'pdf_template.html'
+        context = {'logo': LogoModel.objects.get(id=1)}
+        # Create a Django response object, and specify content_type as pdf
+        response = HttpResponse(content_type='application/pdf')
+
+        # to directly download the pdf we need attachment
+        # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+        # to view on browser we can remove attachment
+        response['Content-Disposition'] = 'filename="report.pdf"'
+
+        # find the template and render it.
+        template = get_template(template_path)
+        html = template.render(context)
+
+        # create a pdf
+        pisa_status = pisa.CreatePDF(
+            html, dest=response)
+        # if error then show some funy view
+        if pisa_status.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
+        # return HttpResponse(pdf, 'application/pdf')
 
 
 class DownloadPDF(View):
